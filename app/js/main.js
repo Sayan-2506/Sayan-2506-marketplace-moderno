3cdffcac264f28ce02a08b459227eb5b9d6dc675
Sayan-2506 <urustemovsayan25@gmail.com>
2020-04-07 14:19:29 +0600

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "10px",
+    readOnly: true
+  });
+
+  $(".product-slider__inner").slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 4
+  });
+
     var mixer = mixitup('.product__inner-box');
+
+
 });
